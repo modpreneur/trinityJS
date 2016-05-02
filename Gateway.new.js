a510e21f66a1a71b538733361b9f84bb1c3d2571
Zbyněk Fišer <fiser@modpreneur.com>
2016-05-02 23:11:58 +0200

    # Forget to add compiled files to GIT

---
+++ Gateway.new.js
@@ -0,0 +1,268 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.configure = undefined;
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _superagent = require('superagent');
+
+var _superagent2 = _interopRequireDefault(_superagent);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Global configuration
+var config = {
+    timeout: 10000,
+    fileTimeout: 10000
+};
+
+var Gateway = {
+    /**
+     *  normal GET request to defined URL
+     * @param url
+     * @param data
+     * @param successCallback
+     * @param errorCallback
+     */
+    get: function get(url, data, successCallback, errorCallback) {
+        __send(url, 'GET', data, successCallback, errorCallback);
+    },
+    /**
+     * JSON GET request
+     * @param url
+     * @param data
+     * @param successCallback
+     * @param errorCallback
+     */
+    getJSON: function getJSON(url, data, successCallback, errorCallback) {
+        __sendJSON(url, 'GET', data, successCallback, errorCallback);
+    },
+
+    /**
+     * normal POST request
+     * @param url
+     * @param data
+     * @param successCallback
+     * @param errorCallback
+     */
+    post: function post(url, data, successCallback, errorCallback) {
+        __send(url, 'POST', data, successCallback, errorCallback);
+    },
+    /**
+     * JSON POST request
+     * @param url
+     * @param data
+     * @param successCallback
+     * @param errorCallback
+     */
+    postJSON: function postJSON(url, data, successCallback, errorCallback) {
+        __sendJSON(url, 'POST', data, successCallback, errorCallback);
+    },
+    /**
+     * normal PUT request
+     * @param url
+     * @param data
+     * @param successCallback
+     * @param errorCallback
+     */
+    put: function put(url, data, successCallback, errorCallback) {
+        __send(url, 'PUT', data, successCallback, errorCallback);
+    },
+    /**
+     * JSON PUT request
+     * @param url
+     * @param data
+     * @param successCallback
+     * @param errorCallback
+     */
+    putJSON: function putJSON(url, data, successCallback, errorCallback) {
+        __sendJSON(url, 'PUT', data, successCallback, errorCallback);
+    },
+
+    /**
+     * JSON PUT request
+     * @param url
+     * @param data
+     * @param successCallback
+     * @param errorCallback
+     */
+    deleteJSON: function deleteJSON(url, data, successCallback, errorCallback) {
+        __sendJSON(url, 'DELETE', data, successCallback, errorCallback);
+    },
+
+    /**
+     * Same as others, just allow specify method.
+     * @param url
+     * @param method
+     * @param data
+     * @param successCallback
+     * @param errorCallback
+     */
+    send: __send,
+
+    /**
+     * Send JSON request and accepts only json
+     * @param url
+     * @param method
+     * @param data
+     * @param successCallback
+     * @param errorCallback
+     */
+    sendJSON: __sendJSON,
+
+    /**
+     * Send file fnc
+     * @param url
+     * @param method
+     * @param file
+     * @param fieldName
+     * @param successCallback
+     * @param errorCallback
+     * @param progressCallback
+     * @private
+     */
+    sendFile: __sendFile,
+
+    /**
+     * settings for gateway
+     */
+    settings: config
+};
+
+/** PRIVATE METHODS **/
+/**
+ * private abstract send request method
+ * @param url
+ * @param method
+ * @param data
+ * @param successCallback
+ * @param errorCallback
+ * @returns {Xhr}
+ * @private
+ */
+function __send(url, method, data, successCallback, errorCallback) {
+    method = method.toUpperCase();
+    var r = (0, _superagent2.default)(method, url.trim()).set('X-Requested-With', 'XMLHttpRequest').timeout(Gateway.settings.timeout);
+
+    if (data) {
+        if (method === 'GET' && !(window.FormData && data instanceof window.FormData)) {
+            r.query(data);
+        } else {
+            r.send(data);
+        }
+    }
+
+    r.end(function (err, response) {
+        if (err) {
+            errorCallback(err);
+        } else {
+            successCallback(response);
+        }
+    });
+}
+
+/**
+ * Private abstract send JSON request method
+ * @param url
+ * @param method
+ * @param data
+ * @param successCallback
+ * @param errorCallback
+ * @returns {Xhr}
+ * @private
+ */
+function __sendJSON(url, method, data, successCallback, errorCallback) {
+    method = method.toUpperCase();
+    var r = (0, _superagent2.default)(method, url.trim()).set({
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+    }).timeout(Gateway.settings.timeout);
+
+    if (data) {
+        if (method === 'GET') {
+            r.query(data);
+        } else {
+            r.send(data);
+        }
+    }
+
+    r.end(function (err, response) {
+        if (err) {
+            errorCallback(err);
+        } else {
+            successCallback(response);
+        }
+    });
+}
+
+/**
+ * Send file fnc
+ * @param url
+ * @param method
+ * @param file
+ * @param fieldName
+ * @param successCallback
+ * @param errorCallback
+ * @param progressCallback
+ * @private
+ */
+function __sendFile(url, method, file, fieldName, successCallback, errorCallback, progressCallback) {
+    method = method || 'POST';
+    fieldName = fieldName || 'files';
+    successCallback = successCallback || _lodash2.default.noop;
+    errorCallback = errorCallback || _lodash2.default.noop;
+
+    var r = (0, _superagent2.default)(method.toUpperCase(), url.trim()).set('X-Requested-With', 'XMLHttpRequest').timeout(config.fileTimeout);
+
+    if (_lodash2.default.isArrayLike(file)) {
+        _lodash2.default.each(file, function (f) {
+            r.attach(fieldName, f);
+        });
+    } else {
+        r.attach(fieldName, file);
+    }
+    if (progressCallback) {
+        r.on('progress', progressCallback);
+    }
+    r.end(function (err, response) {
+        if (err) {
+            errorCallback(err);
+        } else {
+            successCallback(response);
+        }
+    });
+}
+
+/**
+ * Dump response on screen
+ * @param response
+ * @private
+ */
+function _dumpOnScreen(response) {
+    Debug.dump(response);
+}
+
+/**
+ * Set global configuration object
+ * @param conf
+ * @returns {{timeout: number}}
+ */
+function configure(conf) {
+    var keys = Object.keys(conf),
+        keysLength = keys.length;
+
+    for (var i = 0; i < keysLength; i++) {
+        config[keys[i]] = conf[keys[i]];
+    }
+    return config;
+}
+
+/** EXPORTS **/
+exports.default = Gateway;
+exports.configure = configure;
---
+++ utils/Events.js
@@ -0,0 +1,131 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var Events = {
+  /**
+   * Abbreviation for target.addEventListener
+   * @param target {HTMLElement}
+   * @param event {string}
+   * @param callback {function}
+   * @param capture [boolean]
+   * @returns {function}
+   */
+  on: __listen,
+
+  /**
+   * Abbreviation for target.addEventListener
+   * @param target {HTMLElement}
+   * @param event {string}
+   * @param callback {function}
+   * @param capture [boolean]
+   * @returns {function}
+   */
+  listen: __listen,
+
+  /**
+   * Adds wrapper around function and after first invocation, remove listener
+   * @Note Returns correct listener for removing
+   * @param target {HTMLElement}
+   * @param event {string}
+   * @param callback {function}
+   * @param capture [boolean]
+   * @returns {function}
+   */
+  once: __listenOnce,
+
+  /**
+   * Adds wrapper around function and after first invocation, remove listener
+   * @Note Returns correct listener for removing
+   * @param target {HTMLElement}
+   * @param event {string}
+   * @param callback {function}
+   * @param capture [boolean]
+   * @returns {function}
+   */
+  listenOnce: __listenOnce,
+
+  /**
+   * Abbreviation for target.removeEventListener
+   * @param target {HTMLElement}
+   * @param event {string}
+   * @param callback {function}
+   * @param capture [boolean]
+   * @returns {void}
+   */
+  off: __removeListener,
+
+  /**
+   * Abbreviation for target.removeEventListener
+   * @param target {HTMLElement}
+   * @param event {string}
+   * @param callback {function}
+   * @param capture [boolean]
+   * @returns {void}
+   */
+  unlisten: __removeListener,
+
+  /**
+   * Abbreviation for target.removeEventListener
+   * @param target {HTMLElement}
+   * @param event {string}
+   * @param callback {function}
+   * @param capture [boolean]
+   * @returns {void}
+   */
+  removeListener: __removeListener
+};
+
+/**
+ * Abbreviation for target.addEventListener
+ * @param target {HTMLElement}
+ * @param event {string}
+ * @param callback {function}
+ * @param capture [boolean]
+ * @returns {function}
+ * @private
+ */
+function __listen(target, event, callback, capture) {
+  capture = capture || false;
+  target.addEventListener(event, callback, capture);
+  return callback;
+}
+
+/**
+ * Adds wrapper around function and after first invocation, remove listener
+ * @Note Returns correct listener for removing
+ * @param target {HTMLElement}
+ * @param event {string}
+ * @param callback {function}
+ * @param capture [boolean]
+ * @private
+ * @returns {function}
+ */
+function __listenOnce(target, event, callback, capture) {
+  capture = capture || false;
+  var wrapper = function () {
+    return function (e) {
+      target.removeEventListener(event, wrapper, capture);
+      callback(e);
+    };
+  }();
+  target.addEventListener(event, wrapper, capture);
+  return wrapper;
+}
+
+/**
+ * Abbreviation for target.removeEventListener
+ * @param target {HTMLElement}
+ * @param event {string}
+ * @param callback {function}
+ * @param capture [boolean]
+ * @returns {void}
+ * @private
+ */
+function __removeListener(target, event, callback, capture) {
+  capture = capture || false;
+  return target.removeEventListener(event, callback, capture);
+}
+
+exports.default = Events;
